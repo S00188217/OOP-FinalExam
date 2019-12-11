3d4d861ec173f83b42737514baa84b1a3a73c1cb
S00188217 <S00188217@LAB-B1041-13>
2019-12-11 10:36:05 +0000

    First Commit

--- FinalExam/MainWindow.xaml.cs
+++ FinalExam/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace FinalExam
 {
@@ -20,9 +21,82 @@ namespace FinalExam
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Activities> AllActivities;
+        ObservableCollection<Activities> CertainActivities = new ObservableCollection<Activities>();
+        ObservableCollection<Activities> FilteredActivities = new ObservableCollection<Activities>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            listbox1.ItemsSource = ;
+
+            bool interfere = false;
+            Activities ChosenActivity = listbox1.SelectedItem as Activities;
+            foreach (Activities activities in CertainActivities)
+            {
+                if (activities.Date == ChosenActivity.Date)
+                {
+                    interfere = true;
+                    MessageBox.Show("You already have a full schedule for that day");
+                }
+            }
+            if (interfere == false)
+            {
+                CertainActivities.Add(ChosenActivity);
+                AllActivities.Remove(ChosenActivity);
+            }
+
+            if (Radio_All.IsChecked == true)
+            {
+                listbox1.ItemsSource = AllActivities;
+            }
+            else if (Radio_Land.IsChecked == true)
+            {
+                FilteredActivities.Clear();
+
+                foreach (Activities act in AllActivities)
+                {
+                    if (act._ActivityType == Activities.ActivityType.Land)
+                        FilteredActivities.Add(act);
+                }
+
+                listbox1.ItemsSource = FilteredActivities;
+            }
+
+            else if (Radio_Water.IsChecked == true)
+            {
+                FilteredActivities.Clear();
+
+                foreach (Activities act in AllActivities)
+                {
+                    if (act._ActivityType == Activities.ActivityType.Water)
+                        FilteredActivities.Add(act);
+                }
+
+                listbox1.ItemsSource = FilteredActivities;
+
+            }
+
+            else if (Radio_Air.IsChecked == true)
+            {
+                FilteredActivities.Clear();
+
+                foreach (Activities act in AllActivities)
+                {
+                    if (act._ActivityType == Activities.ActivityType.Air)
+                        FilteredActivities.Add(act);
+                }
+
+                listbox1.ItemsSource = FilteredActivities;
+            }
+
+            // Adds the total cost when items are added to listbox2
+
+            total = total + ChosenActivity.Cost;
+            cost.Text = total.ToString();
         }
     }
 }
---
+++ FinalExam/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam
+{
+    public class Player : IComparable
+    {
+        public enum Position { All, Goalkeeper, Defender, Midfielder, Forward }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Position _Position { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+
+
+        public Player(string _firstname, string _lastname, Position _position, string _prefferedposition, DateTime _dateofbirth)
+        {
+            FirstName = _firstname;
+            LastName = _lastname;
+            _Position = _position;
+            DateOfBirth = _dateofbirth;
+        }
+
+        public Player(string _firstname, string _lastname, Position _position, DateTime _dateofbirth) : this(_firstname, _lastname, _position, DateTime.Now)
+        {
+            FirstName = _firstname;
+            LastName = _lastname;
+            _Position = _position;
+            DateOfBirth = _dateofbirth;
+        }
+
+        public Player() : this("", "", Position.All, "", DateTime.Now) { }
+
+        public void DisplayPosition()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} - {DateOfBirth.ToShortDateString()}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            Player that = (Player)obj;
+            return DateOfBirth.CompareTo(that.DateOfBirth);
+        }
+
+
+    }
+
+}
